d87016153a9d1df777b648823d04f2ffe0d5b741
Musheer <musheerakhan121@gmail.com>
2025-03-25 05:56:53 +0530

    Update feature1.ts

--- src/stepDefinations/feature1.ts
+++ src/stepDefinations/feature1.ts
@@ -1,7 +1,6 @@
 import { Given } from '@cucumber/cucumber';
 import { ICustomWorld } from '../support/cucumberWorld';
 import { saveScreenshot } from '../support/utilities';
-import { until } from 'selenium-webdriver';
 
 Given('Hello', async function (this: ICustomWorld) {
     console.log('Hello was called');
